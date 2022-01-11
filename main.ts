4c70ac268afb3fa750c9dcbdc336a050bac15010
bilbobessac <74983423+bilbobessac@users.noreply.github.com>
2022-01-11 20:43:43 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,19 @@
-
+let nbre_pas = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(nbre_pas)
+})
+input.onButtonPressed(Button.AB, function () {
+    nbre_pas = 0
+})
+input.onButtonPressed(Button.B, function () {
+    if (nbre_pas < 8000) {
+        basic.showString("Encore un effort...")
+    } else {
+        basic.showString("BRAVO")
+    }
+})
+basic.forever(function () {
+    if (input.isGesture(Gesture.Shake) || input.isGesture(Gesture.LogoDown)) {
+        nbre_pas += 1
+    }
+})
